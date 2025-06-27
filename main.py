acd51b8578d61e7cfdba5513f0b9278a93153c45
Izumi Hoshino <iz-hoshino@toyokeizai.co.jp>
2025-06-27 18:06:31 +0900

    微修正

--- main.py
+++ main.py
@@ -41,22 +41,22 @@ mcp = FastMCP(
 
 
 @mcp.tool()
-def read_pdf(pdf_path: Path, output_path: Path | None = None) -> str:
+def read_pdf(pdf_path: Path, output_absolute_path: Path | None = None) -> str:
     """Read a PDF file and return the text content
     Args:
         pdf_path: The full path to the PDF file
-        output_path: Optional. The full path to the output file. If provided, the text content will be saved to the path.
+        output_absolute_path: Optional. The full path to the output file. If provided, the text content will be saved to the path.
     Returns:
         The text content of the pdf will be returned.
     """
-    return read_pdf_tool(pdf_path, output_path)
+    return read_pdf_tool(pdf_path, output_absolute_path)
 
 
 @mcp.tool()
-def get_page_rect(pdf_path: Path) -> dict[str, int]:
+def get_page_rect(pdf_absolute_path: Path) -> dict[str, int]:
     """Get the common rectangle of pages of the PDF file. This is useful when cropping rectangle of the pdf since the cropping rectangle must be within the page rectangle.
     Args:
-        pdf_path: The full path to the PDF file
+        pdf_absolute_path: The full path to the PDF file
     Returns:
         The common rectangle of pages of the pdf will be returned. The format is:
         {
@@ -66,75 +66,77 @@ def get_page_rect(pdf_path: Path) -> dict[str, int]:
             height: int,
         }
     """
-    return get_doc_info_tool(pdf_path)
+    return get_doc_info_tool(pdf_absolute_path)
 
 
 @mcp.tool()
 def get_cropped_pdf(
-    pdf_path: Path,
+    pdf_absolute_path: Path,
     page_num: int,
     x: int,
     y: int,
     width: int,
     height: int,
-    output_path: Path | None = None,
+    output_absolute_path: Path | None = None,
 ) -> str:
     """Crop a PDF file and return the cropped pdf. The cropping rectangle must be within the page rectangle.
     Args:
-        pdf_path: The full path to the PDF file
+        pdf_absolute_path: The full path to the PDF file
         page_num: The page number to crop. Starts from 0. (page_num=0 means the first page)
         x: The top-left x coordinate of the crop box
         y: The top-left y coordinate of the crop box
         width: The width of the crop box
         height: The height of the crop box
-        output_path: Optional. The full path to the output file. If provided, the cropped image will be saved to the path.
+        output_absolute_path: Optional. The full path to the output file. If provided, the cropped image will be saved to the path.
     Returns:
         The cropped markdown text of the pdf will be returned.
     """
-    return crop_pdf_tool(pdf_path, page_num, x, y, width, height, output_path)
+    return crop_pdf_tool(
+        pdf_absolute_path, page_num, x, y, width, height, output_absolute_path
+    )
 
 
 @mcp.tool()
 def get_pdf_image(
-    pdf_path: Path,
+    pdf_absolute_path: Path,
     page_num: int,
-    output_path: Path | None = None,
+    output_absolute_path: Path | None = None,
 ) -> Image:
     """Get the image of a PDF file
     Args:
-        pdf_path: The full path to the PDF file
+        pdf_absolute_path: The full path to the PDF file
         page_num: The page number to get the image from. Starts from 0. (page_num=0 means the first page)
-        output_path: Optional. The full path to the output file. If provided, the image will be saved to the path.
+        output_absolute_path: Optional. The full path to the output file. If provided, the image will be saved to the path.
     Returns:
         The image of the pdf is returned.
     """
-    return get_pdf_image_tool(pdf_path, page_num, output_path)
+    return get_pdf_image_tool(pdf_absolute_path, page_num, output_absolute_path)
 
 
 @mcp.tool()
 def get_cropped_pdf_image(
-    pdf_path: Path,
+    pdf_absolute_path: Path,
     page_num: int,
     x: int,
     y: int,
     width: int,
     height: int,
-    output_path: Path | None = None,
+    output_absolute_path: Path | None = None,
 ) -> Image:
     """Get the cropped image of a PDF file. The cropping rectangle must be within the page rectangle.
     Args:
-        pdf_path: The full path to the PDF file
+        pdf_absolute_path: The full path to the PDF file
         page_num: The page number to get the image from. Starts from 0. (page_num=0 means the first page)
         x: The top-left x coordinate of the crop box
         y: The top-left y coordinate of the crop box
         width: The width of the crop box
         height: The height of the crop box
-        output_path: Optional. The full path to the output file. If provided, the cropped image will be saved to the path.
+        output_absolute_path: Optional. The full path to the output file. If provided, the cropped image will be saved to the path.
     Returns:
         The cropped image will be returned.
     """
     return get_cropped_pdf_image_tool(
-        pdf_path, page_num, x, y, width, height, output_path
+        pdf_absolute_path, page_num, x, y, width, height, output_absolute_path
     )
 
 
